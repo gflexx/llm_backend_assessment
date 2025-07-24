89a8c37a386a82221a01d49c293f1be68aa32db3
gflexx <glennenri24@gmail.com>
2025-07-24 14:40:08 +0300

    Updated gitignore routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,2 +1,12 @@
 from .schemas import UserRequest, LLMResponse
+from fastapi import APIRouter
 
+router = APIRouter()
+
+
+@router.post("/chat", response_model=LLMResponse)
+async def ask_question(data: UserRequest):
+    """
+    takes userinput then requests LLM
+    """
+    return {"answer": f"You asked: {data.question}"}
